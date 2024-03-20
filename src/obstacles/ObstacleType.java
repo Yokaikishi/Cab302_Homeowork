7c117d180e48c0a20bce7f166724cf1e422bd57e
Yokaikishi <137606850+Yokaikishi@users.noreply.github.com>
2024-03-20 14:32:26 +1000

    Added other obstacle files(Sensor, Camera, Fence)

--- src/obstacles/ObstacleType.java
+++ src/obstacles/ObstacleType.java
@@ -1,7 +1,10 @@
 package obstacles;
 
 public enum ObstacleType {
-    GUARD("g", 'g');
+    GUARD("g", 'g'),
+    FENCE("f", 'f'),
+    SENSOR("s", 's'),
+    CAMERA("c", 'c');
 
     private final String argumentName;
     private final char symbol;
